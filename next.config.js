b2f98270a65019615924be00c440afe7fc8c8ea6
Tyler Dao <tyler@curafi.com>
2025-12-30 02:24:42 -0800

    config

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
     unoptimized: true,  // required for static export
   },
   distDir: "out", 
-
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/piwwo-draw/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/piwwo-draw' : '',
 };
 
 export default nextConfig;
